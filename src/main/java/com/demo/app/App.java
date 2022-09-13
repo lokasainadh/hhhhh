6019cdf02d6f1a662c5c264c04b909c8281c1c27
lokasainadh <102881545+lokasainadh@users.noreply.github.com>
2022-09-13 09:58:34 +0530

    update

--- src/main/java/com/demo/app/App.java
+++ src/main/java/com/demo/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! This is new Maven. Hi" );
+        System.out.println( "Hello World! This is a new Maven. Hi" );
     }
 }
